cf1e29fe04ed58fea4b66bff5312eeb2716797c4
MaciejZientara <mtzientara@gmail.com>
2024-12-13 22:50:33 +0100

    fix patient service

--- wsb-jpa1/src/main/java/com/jpacourse/service/impl/PatientServiceImpl.java
+++ wsb-jpa1/src/main/java/com/jpacourse/service/impl/PatientServiceImpl.java
@@ -4,13 +4,14 @@ import com.jpacourse.dto.PatientTO;
 import com.jpacourse.mapper.PatientMapper;
 import com.jpacourse.persistence.dao.PatientDao;
 import com.jpacourse.persistence.entity.PatientEntity;
+import com.jpacourse.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class PatientServiceImpl
+public class PatientServiceImpl implements PatientService
 {
     private final PatientDao patientDao;
 
