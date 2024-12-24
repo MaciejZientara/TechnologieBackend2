904be3f8098cdc161bc0fe79008690c558d26e0a
MaciejZientara <mtzientara@gmail.com>
2024-12-24 13:01:00 +0100

    addVisit in PatientDAO, to test

--- wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/PatientDao.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/PatientDao.java
@@ -2,5 +2,8 @@ package com.jpacourse.persistence.dao;
 
 import com.jpacourse.persistence.entity.PatientEntity;
 
+import java.time.LocalDateTime;
+
 public interface PatientDao extends Dao<PatientEntity, Long> {
+    void addVisit(long patientId, long doctorId, LocalDateTime time, String description);
 }
--- wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/PatientDaoImpl.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/PatientDaoImpl.java
@@ -2,9 +2,32 @@ package com.jpacourse.persistence.dao.impl;
 
 
 import com.jpacourse.persistence.dao.PatientDao;
+import com.jpacourse.persistence.entity.DoctorEntity;
 import com.jpacourse.persistence.entity.PatientEntity;
+import com.jpacourse.persistence.entity.VisitEntity;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public class PatientDaoImpl  extends AbstractDao<PatientEntity, Long> implements PatientDao {
+
+    AbstractDao<DoctorEntity,Long> doctorDao;
+
+    @Override
+    public void addVisit(long patientId, long doctorId, LocalDateTime time, String description)
+    {
+        PatientEntity patient = getOne(patientId);
+
+        VisitEntity newVisit = new VisitEntity();
+        newVisit.setDescription(description);
+        newVisit.setTime(time);
+        newVisit.setDoctor(doctorDao.getOne(doctorId));
+        newVisit.setPatient(patient);
+
+        List<VisitEntity> result = patient.getVisits();
+        result.add(newVisit);
+        patient.setVisits(result);
+    }
 }
